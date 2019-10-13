55c4471bcdac8a4ad2e1bb0c5b88194496373bdd
John <ibortsov14@gmail.com>
2019-10-13 17:13:19 +0300

    Add basic config and update composer.json

---
+++ config/config.example.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ROOT', dirname(__DIR__));
+define('IMAGES', ROOT . '/images');
+
+define('CONFIRM_TOKEN', 'XXXXXXXXX');
+define('TOKEN', 'XXXXXXXXXXXXXXXXX');
+define('SECRET_KEY', 'XXXXXXXXXXXX');
