10303d1298cf076699705dad59066317d0a6a13f
John <ibortsov14@gmail.com>
2019-10-13 19:57:29 +0300

    Update basic config

--- config/config.example.php
+++ config/config.example.php
@@ -3,6 +3,7 @@
 define('ROOT',     dirname(__DIR__));
 define('IMAGES',   ROOT . '/images');
 define('SESSIONS', ROOT . '/sessions');
+define('LOGS',     ROOT . '/logs');
 
 define('CONFIRM_TOKEN', 'XXXXXXXXX');
 define('TOKEN',         'XXXXXXXXXXXXXXXXX');
