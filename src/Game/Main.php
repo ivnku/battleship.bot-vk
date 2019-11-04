eed0f5e2476d59afa48fd0a4632ca4023caa065e
John <ibortsov14@gmail.com>
2019-11-04 23:08:13 +0300

    Add Player and Main classes

---
+++ src/Game/Main.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BSBot\Game;
+
+
+class Main
+{
+    
+}
---
+++ src/Game/Player.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BSBot\Game;
+
+
+class Player
+{
+    /**
+     * VK id of a player
+     * @var string
+     */
+    public $vk_id;
+    
+    /**
+     * String like "Б2"
+     * @var string 
+     */
+    public $move;
+    
+    /**
+     * Field object which contains player's field and 
+     * array containing arrays of ships' coordinates
+     * @var Field 
+     */
+    public $field;
+    
+    /**
+     * Two dimensional array of player's opponent field
+     * @var array 
+     */
+    public $opponent_field;
+    
+    /**
+     * The last move on the opponent field
+     * @var array 
+     */
+    public $last_action;
+    
+    public function __construct(string $vk_id, string $move, Field $field, array $opponent_field, array $last_action)
+    {
+        $this->vk_id          = $vk_id;
+        $this->move           = $move;
+        $this->field          = $field;
+        $this->opponent_field = $opponent_field;
+        $this->last_action    = $last_action;
+    }
+}
