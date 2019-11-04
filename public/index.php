a72367c0c1de68f39a6dc831d70a485029bae69f
John <ibortsov14@gmail.com>
2019-11-04 23:17:30 +0300

    minor changes

--- public/index.php
+++ public/index.php
@@ -2,10 +2,23 @@
 require_once dirname(__DIR__) . '/config/config.php';
 require_once ROOT . '/vendor/autoload.php';
 
+use BSBot\Game\Field;
 
 if ($_SERVER['REQUEST_URI'] == '/confirmation') {
     echo CONFIRM_TOKEN;
 } else {
-    $data = json_decode(file_get_contents('php://input'));
-    var_dump($data);  
+//    $data = json_decode(file_get_contents('php://input'));
+//    var_dump($data);
+    
+    $bot = new Field();
+    $bot->generateShips('141414');
+    
+    for ($i = 0; $i < 10; $i++) {
+        foreach ($bot->map as $letter) {
+            echo $letter[$i] . ' ';
+        }
+        echo '<br>';
+    }
+    echo '<br>';
+    print_r($bot->ships);
 }
